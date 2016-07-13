2da57190f1a5b81a2a7c05631d566de495a6e25c
Greg Brown <greg@12dnetworks.com>
2016-07-13 09:45:28 -0700

    wip

--- src/Controller/DeviceViewRendererTrait.php
+++ src/Controller/DeviceViewRendererTrait.php
@@ -5,23 +5,8 @@ namespace Gdbots\Bundle\AppBundle\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-trait DeviceViewAwareTemplateRendering
+trait DeviceViewRendererTrait
 {
-    /**
-     * @param string $id
-     * @return object
-     */
-    abstract protected function get($id);
-
-    /**
-     * @param string $view
-     * @param array $parameters
-     * @param Response|null $response
-     *
-     * @return Response
-     */
-    abstract protected function render($view, array $parameters = [], Response $response = null);
-
     /**
      * Renders a view that is device view specific first, if found, otherwise, the default/shared view.
      * This calls the default FrameworkBundle Controller "render" method under the hood.
@@ -29,15 +14,20 @@ trait DeviceViewAwareTemplateRendering
      * @param string   $view       The view name (with %device_view% string which will be replaced with current value)
      * @param array    $parameters An array of parameters to pass to the view
      * @param Response $response   A response instance
+     * @param Request  $request    A request instance
      *
      * @return Response A Response instance
      *
      * @throws \Exception
      */
-    protected function deviceViewAwareRender($view, array $parameters = [], Response $response = null)
-    {
+    protected function renderUsingDeviceView(
+        $view,
+        array $parameters = [],
+        Response $response = null,
+        Request $request = null
+    ) {
         /** @var Request $request */
-        $request = $this->get('request_stack')->getCurrentRequest();
+        $request = $request ?: $this->get('request_stack')->getCurrentRequest();
         $deviceView = $request->get('device_view');
 
         if (null === $deviceView) {
@@ -54,4 +44,19 @@ trait DeviceViewAwareTemplateRendering
             return $this->render(str_replace('%device_view%', '', $view), $parameters, $response);
         }
     }
+
+    /**
+     * @param string $id
+     * @return object
+     */
+    abstract protected function get($id);
+
+    /**
+     * @param string $view
+     * @param array $parameters
+     * @param Response $response
+     *
+     * @return Response
+     */
+    abstract protected function render($view, array $parameters = [], Response $response = null);
 }
