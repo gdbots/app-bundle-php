a98abde7b6a23861da8d8bf9f6a2db29ff527cac
Greg Brown <greg@12dnetworks.com>
2017-02-12 18:40:48 -0800

    get device view from attributes only and remove need to pass request object

--- src/Controller/DeviceViewRendererTrait.php
+++ src/Controller/DeviceViewRendererTrait.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types = 1);
 
 namespace Gdbots\Bundle\AppBundle\Controller;
 
@@ -8,34 +9,29 @@ use Symfony\Component\HttpFoundation\Response;
 trait DeviceViewRendererTrait
 {
     /**
-     * Renders a view that is device view specific first, if found, otherwise, the default/shared view.
-     * This calls the default FrameworkBundle Controller "render" method under the hood.
+     * Renders a view that is device view specific first, if found, otherwise the default/shared view.
+     * This calls the FrameworkBundle Controller "render" method under the hood.
      *
      * @param string   $view       The view name (with %device_view% string which will be replaced with current value)
      * @param array    $parameters An array of parameters to pass to the view
      * @param Response $response   A response instance
-     * @param Request  $request    A request instance
      *
      * @return Response A Response instance
      *
      * @throws \Exception
      */
-    protected function renderUsingDeviceView(
-        $view,
-        array $parameters = [],
-        Response $response = null,
-        Request $request = null
-    ) {
+    protected function renderUsingDeviceView(string $view, array $parameters = [], ?Response $response = null): Response
+    {
         /** @var Request $request */
-        $request = $request ?: $this->get('request_stack')->getCurrentRequest();
-        $deviceView = $request->get('device_view');
+        $request = $this->get('request_stack')->getCurrentRequest();
+        $deviceView = $request ? $request->attributes->get('device_view') : null;
 
-        if (null === $deviceView) {
+        if (null === $deviceView || false === strpos($view, '%device_view%')) {
             return $this->render(str_replace('%device_view%', '', $view), $parameters, $response);
         }
 
         try {
-            return $this->render(str_replace('%device_view%', '.'.$deviceView, $view), $parameters, $response);
+            return $this->render(str_replace('%device_view%', '.' . $deviceView, $view), $parameters, $response);
         } catch (\Exception $e) {
             if (!$e->getPrevious() instanceof \Twig_Error_Loader) {
                 throw $e;
@@ -47,13 +43,14 @@ trait DeviceViewRendererTrait
 
     /**
      * @param string $id
+     *
      * @return object
      */
     abstract protected function get($id);
 
     /**
-     * @param string $view
-     * @param array $parameters
+     * @param string   $view
+     * @param array    $parameters
      * @param Response $response
      *
      * @return Response
